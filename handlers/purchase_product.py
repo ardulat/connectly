c691bdce1bf5befbba132fd2f4921aea75d38c1e
Anuar <ardulat@gmail.com>
2022-05-24 12:42:28 +0300

    feat: enrich purchase_product handler

--- handlers/purchase_product.py
+++ handlers/purchase_product.py
@@ -1,14 +1,59 @@
+import random
 from models.handler import BaseHandler
 from models.response import Response
 
 
+PRODUCTS_DATABASE = {
+    'book': [
+        '1984 by by George Orwell',
+        'A Brief History of Time by Stephen Hawking',
+        'Dune (Dune Chronicles, Book 1) by Frank Herbert',
+        'Fahrenheit 451 by Ray Bradbury',
+        'Harry Potter and the Sorcerer\'s Stone by J.K. Rowling',
+        'The Hunger Games (Book 1) by Suzanne Collins',
+    ],
+    'sofa': [
+        'green',
+        'blue',
+        'white',
+        'yellow',
+        'brown',
+    ],
+}
+
+
 class PurchaseProductHandler(BaseHandler):
     def __init__(self):
-        pass
+        print("Connecting to products database.")
 
-    def __is_available(product):
-        pass
+    def __is_available(self, product_type, product_name):
+        if product_type not in PRODUCTS_DATABASE:
+            return False
+        if product_name not in PRODUCTS_DATABASE[product_type]:
+            return False
+        return True
+
+    def __get_product_attributes(self, query):
+        product_type = ''
+        product_name = ''
+
+        if 'book' in query:
+            product_type = 'book'
+        elif 'sofa' in query:
+            product_type = 'sofa'
+
+        if product_type:
+            product_name = random.choice(PRODUCTS_DATABASE[product_type])
+
+        return product_type, product_name
 
     def handle(self, form, query):
-        response = Response('purchasing product')
-        return response
+        # TODO(ardulat): get product_type and product_name from form/frame filler
+        # for now, let's just use some workaround
+        product_type, product_name = self.__get_product_attributes(query)
+        print("Retrieved the following product attributes: product_type - {}, product_name - {}.".format(product_type, product_name))
+
+        if self.__is_available(product_type, product_name):
+            return Response('Yes, we have it in stock. I will redirect you to our operators to place an order.')
+
+        return Response('Sorry, we don\'t have it now. Maybe I can notify you when it\'s available?')
